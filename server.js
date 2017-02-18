9703b65f277fe736ac48ba832deb9fb76f7c0f42
nirajjadhav <nirajjadhav5596@gmail.com>
2017-02-18 12:01:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ app.get('/article-one', function (req, res) {
   res.send('Article one is req here and served');
 });
 
+app.get('/article-two', function (req, res) {
+  res.send('Article two is req here and served');
+});
+
+app.get('/article-three', function (req, res) {
+  res.send('Article three is req here and served');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
